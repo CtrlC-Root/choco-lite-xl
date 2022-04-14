90a46f6cf6a577fbdb724ceb53467e76566a4899
Not-a-web-developer <newestideaz@gmail.com>
2022-04-15 01:24:02 +0530

    dammit how do i keep messing this up

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -8,12 +8,12 @@ $packageArgs = @{
   unzipLocation   = $toolsDir
   specificFolder  = 'lite-xl'
 
-  url             = 'https://github.com/lite-xl/lite-xl/releases/download/v2.0.3/lite-xl-windows-x86.zip'
+  url             = 'https://github.com/lite-xl/lite-xl/releases/download/v2.0.5/lite-xl-windows-x86.zip'
   checksum        = '4587FE52B1B4E8A181F41B694F17867C265D31BC938D0C1C151883C985C48511'
   checksumType    = 'sha256'
 
-  url64bit        = 'https://github.com/lite-xl/lite-xl/releases/download/v2.0.3/lite-xl-windows-x86_64.zip'
-  checksum64      = 'BD59BBC9B136D39BB086A1EBDEE80ED885CE851CD69AD9F1F62C0A4937A41FAB'
+  url64bit        = 'https://github.com/lite-xl/lite-xl/releases/download/v2.0.5/lite-xl-windows-x86_64.zip'
+  checksum64      = '383005F6E45B102FDAE6F6C0F1468CAB916EDED09AA6C548FB0EB07E94613044'
   checksumType64  = 'sha256'
 }
 
