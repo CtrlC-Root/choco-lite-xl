2bf8ff9c5fcefb742779e456ff5a1b173794ddb7
Alexandru Barbur <alex@ctrlc.name>
2023-01-21 18:19:14 -0500

    Update to version 2.1.1.

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -8,16 +8,12 @@ $packageArgs = @{
   unzipLocation   = $toolsDir
   specificFolder  = 'lite-xl'
 
-##  url             = 'https://github.com/lite-xl/lite-xl/releases/download/v2.1.0/lite-xl-v2.1.0-windows-i686.zip'
-##  checksum        = '6cff99fe707e3be4832de1584b2a44183b6039502f1ec815b87bc78a347fc5bf'
-  url             = 'https://github.com/lite-xl/lite-xl/releases/download/v2.1.0/lite-xl-v2.1.0-addons-windows-i686.zip'
-  checksum        = '52a3ec5f7bca5b2eb3ba8607a47600a8bacd297be8bd611ab8268976884c1323'
+  url             = 'https://github.com/lite-xl/lite-xl/releases/download/v2.1.1/lite-xl-v2.1.1-addons-windows-i686.zip'
+  checksum        = '789f61e8b4fbadaa3a51dc5bbe8f06a7b117d47228a2a7326fea2f51aec2517b'
   checksumType    = 'sha256'
 
-##  url64bit        = 'https://github.com/lite-xl/lite-xl/releases/download/v2.1.0/lite-xl-v2.1.0-windows-x86_64.zip'
-##  checksum64      = 'c9ecc452bdd24c2596532bb1034b1a340d30da7243c5ecb921e731470a02b803'
-  url64bit        = 'https://github.com/lite-xl/lite-xl/releases/download/v2.1.0/lite-xl-v2.1.0-addons-windows-x86_64.zip'
-  checksum64      = '294a482858a68e8ca4cc332f488f693509b176ab2829e63aaf3b82cc74d4fc90'
+  url64bit        = 'https://github.com/lite-xl/lite-xl/releases/download/v2.1.1/lite-xl-v2.1.1-addons-windows-x86_64.zip'
+  checksum64      = 'e1931d6a5c2abedee4b8775aaec5dcaa2f97ab8ccb8f61608995b17486bddc43'
   checksumType64  = 'sha256'
 }
 
